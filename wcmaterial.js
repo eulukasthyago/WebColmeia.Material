64620df68a20e27d5df9a9014a913609fd9065d7
Lucas Tiago <eulukasthyago@outlook.com>
2018-12-06 19:48:11 -0200

    Add

---
+++ wcmaterial.js
@@ -0,0 +1,17 @@
+(function(wcdatacont){
+    let wcdata = document.querySelectorAll("[wcdata]");
+    
+    for(let i in radiusquad){
+        alert("a");
+    }
+
+});
+
+window.onload = function(){
+    let wcdata = document.querySelectorAll("[wcdata]");
+    console.log();
+    
+    for(let i = 0; i < wcdata.length; i++){
+        wcdata[i];
+    }
+}
