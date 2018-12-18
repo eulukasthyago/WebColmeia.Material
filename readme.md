3f57b9d895ae048d5559e79d829fa246749231b3
Lucas Tiago <eulukasthyago@outlook.com>
2018-12-18 15:36:36 -0200

    update

---
+++ readme.md
@@ -0,0 +1,18 @@
+## 	Web Colmeia Material
+
+Gerenciador de Layout baseado no novo material design da **Google**
+
+Versão: 0.0.1
+
+Comando implementado:
+
+```json
+{"radiusquad": true} //Cria uma borda quadrada no canto superior esquendo do elemento.
+{"radiuscircle": true} // Cria uma borda arredondada no canto superior esquerdo do elemento.
+{"radiusinnercircle": true} // Cria uma borda arredondada para dentro no canto superior esquerdo do elemento.
+{"borderradius": true} // Faz com que todo os cantos do elemento fique com uma borda arredondada.
+{"btncircle": true} // Transforma o elemento em um botão arredondado
+{"icon":"iconname"} // adociona icone ao elemento
+{"shadowposition": 1...5} // Define a denciade da sombra
+```
+
--- wcmaterial.js
+++ wcmaterial.js
@@ -6,6 +6,12 @@
 //    }
 //
 //});
+
+let addMaterialIcons = document.createElement('link');
+addMaterialIcons.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+addMaterialIcons.rel = "stylesheet";
+document.head.appendChild(addMaterialIcons);
+
 window.onload = function(){
     let wcdataAll = document.querySelectorAll("[wcdata]");
     
@@ -14,7 +20,29 @@ window.onload = function(){
         for(let a in wcdata){
             if(wcdata[a] === true){
                 wcdataAll[i].classList.add(a);
+                if (a === 'btncircle') {
+                	wcdataAll[i].classList.add('btn');
+                }
+            }else if(a === 'icon'){
+            	if (wcdata[a] === "plus") {
+                    wcdataAll[i].innerHTML += '<i class="material-icons">add</i>';
+                }
+            }else if(a === "shadowposition"){
+            	if (wcdata[a] === 1) {
+            		wcdataAll[i].classList.add('shadowposition1');
+            	}else if(wcdata[a] === 2){
+            		wcdataAll[i].classList.add('shadowposition2');
+            	}else if(wcdata[a] === 3){
+            		wcdataAll[i].classList.add('shadowposition3');
+            	}else if(wcdata[a] === 4){
+            		wcdataAll[i].classList.add('shadowposition4');
+            	}else if(wcdata[a] === 5){
+            		wcdataAll[i].classList.add('shadowposition5');
+            	}else{
+					wcdataAll[i].classList.add('shadowposition1');
+            	}
             }
         }
+        wcdataAll[i].removeAttribute("wcdata");
     }
 };
